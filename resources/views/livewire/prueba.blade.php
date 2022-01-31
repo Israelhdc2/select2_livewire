e3a041f1478b15a60437bd4b4660039ad185b72e
Israelhdc2 <israelhdc2@gmail.com>
2022-01-30 20:00:50 -0500

    creamos y incluimos component prueba, creamos select en su vista

---
+++ resources/views/livewire/prueba.blade.php
@@ -0,0 +1,8 @@
+<div>
+    {{-- The Master doesn't talk, he acts. --}}
+    <select>
+        <option value="PE">Peru</option>
+        <option value="AR">Argentina</option>
+        <option value="CH">Chile</option>
+    </select>
+</div>
