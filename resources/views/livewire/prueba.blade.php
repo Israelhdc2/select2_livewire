8910a3caf3aa00b0ccc890edcd2a1c3739bd3498
Israelhdc2 <israelhdc2@gmail.com>
2022-01-30 20:12:22 -0500

    al code javascript debemo indicar que se exec luego que se haya cargado todo el component, iniciliazamos select2

--- resources/views/livewire/prueba.blade.php
+++ resources/views/livewire/prueba.blade.php
@@ -1,8 +1,15 @@
 <div>
     {{-- The Master doesn't talk, he acts. --}}
-    <select>
+    <select class="select2">
         <option value="PE">Peru</option>
         <option value="AR">Argentina</option>
         <option value="CH">Chile</option>
     </select>
+
+    <script>
+        document.addEventListener('livewire:load', function(){
+            $(".select2").select2();
+        });
+    </script>
+
 </div>
